ad5b74ee4f40daf0683f591970ed0a5e8dba8738
twój login do GitHuba <s.k.kazmierczak7@gmail.com>
2022-10-08 13:00:31 +0200

    zadanie 14

---
+++ 14-slicy.py
@@ -0,0 +1,11 @@
+# Pytanie 14
+# Wypisz pierwsze 5 elementów listy L.
+# Wypisz co drugą literę stringa s, zaczynając od ostatniej i cofając się do poczatku.
+
+L = [11, 22, 33, 44, 55, 66, 77, 88, 99, 1010]
+s = 'a nMozh^tKysPW 9ęxi b$uML'
+
+print(L[:5])
+print(s[::-2])
+
+# https://stackoverflow.com/questions/509211/understanding-slicing
