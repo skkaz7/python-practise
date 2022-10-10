32c0ea50d0c9355aad5cdf0e9d017ae4d40f7a98
twój login do GitHuba <s.k.kazmierczak7@gmail.com>
2022-10-10 13:21:16 +0200

    zadanie 18

---
+++ 18-podzielnosc.py
@@ -0,0 +1,8 @@
+# Pytanie 18 - napisz funkcję, która będzie pobierać dwie liczby
+# i sprawdzać czy pierwsza z nich jest podzielna przez drugą
+
+def func(a, b):
+    return a % b == 0
+
+
+print(func(10, 5))
