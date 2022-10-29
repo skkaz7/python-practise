7befc917f58a5970591d97ebc67fe06a03562d5c
twój login do GitHuba <s.k.kazmierczak7@gmail.com>
2022-10-29 22:00:15 +0200

    zadanie 30

---
+++ 30-is-not-in.py
@@ -0,0 +1,12 @@
+# Pytanie 30 - jaką wartość przyjmie poniższe zdanie logiczne?
+# Wyjaśnij proces jego ewaluacji i znaczenie poszczególnych słów: is, not, in.
+
+print(1 is not True in [1, 2, 3])
+# (1 is not True) and (True in [1,2,3]) # ->
+# (True) and (True) -> True
+
+# różne wyniki dla operatorów "is not" oraz "!=" !
+print(1 is not True)
+print(1 != True)
+
+# https://docs.python.org/3/library/operator.html
