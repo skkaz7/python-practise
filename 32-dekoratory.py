bb36d4909b88ee97ff68bfb06e90b21e5672fa9d
twój login do GitHuba <s.k.kazmierczak7@gmail.com>
2022-10-30 12:48:28 +0100

    zadanie 32

---
+++ 32-dekoratory.py
@@ -0,0 +1,17 @@
+# Pytanie 32 - do czego w Pythonie służą dekoratory? Napisz dekorator, który będzie
+# będzie dodawał trzy gwiazdki przed i po efekcie działania udekorowanej funkcji.
+
+def add_stars(function):
+    def decorated_function():
+        print('***')
+        function()
+        print('***')
+
+    return decorated_function()
+
+
+@add_stars
+def f():
+    print('Hello World!')
+
+# https://stackoverflow.com/questions/5929107/decorators-with-parameters
