50cfc204bc659a2f43e37e20d165ecf932a97c05
skkaz7 <s.k.kazmierczak7@gmail.com>
2022-11-16 21:21:49 +0100

    zadanie-36

---
+++ 36-__str__.py
@@ -0,0 +1,18 @@
+# Pytanie 36 - wykorzystajmy klasę Pies i obiekt maly_pies z poprzedniego pytania.
+# Co stanie się gdy wypiszemy print(maly_pies)?
+# Co zrobiłbyś, aby wydrukowana w ten sposób informacja zawierała imie i rase psa?
+
+class Pies:
+    def __init__(self, imie, rasa):
+        self.imie = imie
+        self.rasa = rasa
+
+    def __str__(self):
+        return f"{self.imie} - {self.rasa}"
+
+
+piesek = Pies('Odi', 'sznaucer')
+print(piesek)
+
+# __str__ metoda specjalna, magiczna, która ma w Pythonie z góry określoną funkcjonalność
+# metoda __str__ służy do tworzenia reprezentacji tekstowej obiektów klasy
