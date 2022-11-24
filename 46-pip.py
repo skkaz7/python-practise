30226f71491064632f2cd5db2b06be1bfa20010c
skkaz7 <s.k.kazmierczak7@gmail.com>
2022-11-24 15:34:01 +0100

    zadanie-46

---
+++ 46-pip.py
@@ -0,0 +1,16 @@
+# Pytanie 46 - w jaki sposób importujesz zewnętrzne moduły w Pythonie?
+
+# pip install nazwa_modułu
+
+from colorama import init, Fore, Back, Style
+
+init()
+print(Fore.RED + "Tekst na czerwono")
+print(Back.YELLOW + "... na żółtym tle")
+print(Style.RESET_ALL + "... i znowu normalnie.")
+
+# PyPI - Python Package Index - strona, na której dostępna jest ogromna ilość pakietów do Pythona wraz z dokumentacją
+# to z niej pip ściąga instalowane przez nas pakiety
+
+# https://pypi.org/
+# https://pypi.org/project/pip/
